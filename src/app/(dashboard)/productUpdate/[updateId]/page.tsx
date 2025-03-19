77307d67b67cfe02fd3e3d3c7629ce83226f199b
Edson Santos <129888422+Edsonsantos0840@users.noreply.github.com>
2025-03-19 16:36:05 -0300

    corrigindo params

--- src/app/(dashboard)/productUpdate/[updateId]/page.tsx
+++ src/app/(dashboard)/productUpdate/[updateId]/page.tsx
@@ -2,8 +2,8 @@ import ProductUpdate from "@/app/(dashboard)/components/form/UpdateProduct";
 import LoadingSuspense from "@/app/(home)/LoadingSuspense";
 import { Suspense } from "react";
 
-export default async function UpdateProduct({ params }: { params: Record<string, string>  }) {
-  const { updateId } = await params;
+export default function UpdateProduct({ params }: { params: Record<string, string>  }) {
+  const { updateId } = params;
 
   return (
     <Suspense fallback={<LoadingSuspense/>}>
