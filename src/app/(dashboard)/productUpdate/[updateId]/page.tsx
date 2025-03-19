280b9d15808d4d25a17011f2114162e9e24d2aaa
Edson Santos <129888422+Edsonsantos0840@users.noreply.github.com>
2025-03-19 16:44:17 -0300

    aplicando correção de erros

--- src/app/(dashboard)/productUpdate/[updateId]/page.tsx
+++ src/app/(dashboard)/productUpdate/[updateId]/page.tsx
@@ -2,7 +2,7 @@ import ProductUpdate from "@/app/(dashboard)/components/form/UpdateProduct";
 import LoadingSuspense from "@/app/(home)/LoadingSuspense";
 import { Suspense } from "react";
 
-export default function UpdateProduct({ params }: { params: Record<string, string>  }) {
+export default function UpdateProduct({ params }: { params: { updateId: string } }) {
   const { updateId } = params;
 
   return (
