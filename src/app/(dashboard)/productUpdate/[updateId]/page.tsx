b37b23e74743cae9872cf01d6478f7506f4ea2cf
Edson Santos <129888422+Edsonsantos0840@users.noreply.github.com>
2025-03-19 16:20:14 -0300

    corrigindo erro no deploy

--- src/app/(dashboard)/productUpdate/[updateId]/page.tsx
+++ src/app/(dashboard)/productUpdate/[updateId]/page.tsx
@@ -2,8 +2,8 @@ import ProductUpdate from "@/app/(dashboard)/components/form/UpdateProduct";
 import LoadingSuspense from "@/app/(home)/LoadingSuspense";
 import { Suspense } from "react";
 
-export default async function UpdateProduct({ params }: { params: { updateId: string } }) {
-  const { updateId } = params;
+export default async function UpdateProduct({ params }: { params: Record<string, string>  }) {
+  const { updateId } = await params;
 
   return (
     <Suspense fallback={<LoadingSuspense/>}>
